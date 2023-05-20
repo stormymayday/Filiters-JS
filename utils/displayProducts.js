508be4714373360b57319cc5126e95d6d8c573d5
stormymayday <stormymayday7@gmail.com>
2023-05-20 09:57:06 -0700

    Dynamically rendering the products using displayProducts function

--- utils/displayProducts.js
+++ utils/displayProducts.js
@@ -2,7 +2,19 @@ import { productsContainer, filteredProducts } from "../app.js";
 
 const displayProducts = () => {
 
-    console.log(filteredProducts);
+    productsContainer.innerHTML = filteredProducts.map(({ id, title, image, price }) => {
+
+        return `          <article class="product" data-id="${id}">
+                <img class="product-img img"
+                    src="${image}"
+                    alt="${title}">
+                <footer>
+                    <h5 class="product-name">${title}</h5>
+                    <span class="product-price">${price}</span>
+                </footer>
+            </article>`;
+
+    }).join('');
 
 };
 
