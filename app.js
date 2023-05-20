20781f81dcc304c9a8e8e7716088626df85b7573
stormymayday <stormymayday7@gmail.com>
2023-05-20 09:41:26 -0700

    Importing displayProducts in app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import { products } from "./utils/products.js";
 import getElement from "./utils/getElement.js";
+import displayProducts from "./utils/displayProducts.js";
 
 // Creating a copy of products array
 let filteredProducts = [...products];
 
 // Selecting 'products-container'
 export const productsContainer = getElement('.products-container');
+
+displayProducts();
