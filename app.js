f7e7d581d34219b55cb2a823fc4245110df69523
stormymayday <stormymayday7@gmail.com>
2023-05-23 07:04:55 -0700

    Adding company buttons to the DOM

--- app.js
+++ app.js
@@ -36,11 +36,15 @@ const companiesDOM = getElement('.companies');
 
 const displayFilterButtons = () => {
 
+    // Getting unique company values and 'all'
     const companyButtons = ['all', ...new Set(products.map((product) => {
         return product.company;
     }))];
 
-    console.log(companyButtons);
+    // Adding company buttons to the DOM
+    companiesDOM.innerHTML = companyButtons.map((company) => {
+        return `<button class="company-btn" data-id="${company}">${company}</button>`;
+    }).join('');
 
 };
 displayFilterButtons();
